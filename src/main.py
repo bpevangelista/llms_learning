9121ca7902fc5d521461b58f8c7b2817b0b71520
Bruno Evangelista <bpevangelista@gmail.com>
2024-02-20 10:33:28 -0800

    Add main with mem & perf test

--- src/main.py
+++ src/main.py
@@ -1,88 +1,64 @@
+import time, torch
 
-import math
-import torch
-import torch.nn as nn
+import utils
+from model_attention_naive import AttentionNaive
+from model_attention_modern import AttentionXformers, AttentionSdpa
+from model_blocks import LayerNorm
 
+torch.set_default_dtype(torch.float16)
+torch.set_default_device('cuda:0')
 
 # Transformer parameters
 vocab_size = 32 * 1024  # 32K words/tokens embeddings in vocabulary
-embedding_dim = 4096    # 4096 embedding dimension (dmodel)
-max_seq_length = 2048   # 2048 maximum input tokens
+max_seq_length = 2048  # 2048 maximum input tokens
+embedding_dim = 4096  # OPT 512, Llama2 4096 embedding dimension (dmodel)
+num_heads = 32  # 16 or 32
 
-def build_absolute_positional_encoding_naive():
-    positions = torch.arange(max_seq_length)  # [0,1,2, ... max_seq_length]
-    positions = positions.unsqueeze(1)  # [0,1,2, ... max_seq_length][]
-    embeddings = torch.arange(embedding_dim)  # [0,1,2, ... embedding_dim]
-
-    # pos/10000^(2i/dmodel)
-    angle = positions / torch.pow(10000, (2 * embeddings / embedding_dim))
-
-    # PE(pos, 2i+0) = sin( pos/10000^(2i/dmodel) )
-    # PE(pos, 2i+1) = cos( pos/10000^(2i/dmodel) )
-    positional_encoding = torch.empty(max_seq_length, embedding_dim)
-    positional_encoding[:, 0::2] = torch.sin(angle[:, 0::2])
-    positional_encoding[:, 1::2] = torch.cos(angle[:, 1::2])
-    return positional_encoding
+mha_naive = AttentionNaive(embedding_dim, num_heads)
+mha_xformers = AttentionXformers(embedding_dim, num_heads)
+mha_sdpa = AttentionSdpa(embedding_dim, num_heads)
+norm_layer = LayerNorm(embedding_dim)
 
 
-# q, k, v – layers were learned during training
-q = nn.Linear(embedding_dim, embedding_dim, bias=False)
-k = nn.Linear(embedding_dim, embedding_dim, bias=False)
-v = nn.Linear(embedding_dim, embedding_dim, bias=False)
+def test_attn(hidden_states: torch.Tensor, attention_obj: any, layers: int, steps: int, title: str | None):
+    memory_usage = torch.cuda.max_memory_allocated() / 1024 ** 2
+    print(f'\n{title:16} START Peak CUDA Mem: {memory_usage:.2f} MB')
 
-def multi_head_attention(
-        q_states : torch.Tensor,
-        k_states: torch.Tensor,
-        v_states: torch.Tensor,
-        ) -> torch.Tensor:
-    
+    start = time.perf_counter()
+    hidden = None
+    for step in range(steps):
+        hidden = hidden_states.clone()
+        for layer in range(layers):
+            hidden = attention_obj(norm_layer(hidden), None) + hidden
+    end = time.perf_counter()
 
-def multi_head_attention_naive(input_embd: torch.Tensor,
-                               num_heads: int) -> torch.Tensor:
-    q1 = q(input_embd)
-    k1 = k(input_embd)
-    v1 = v(input_embd)
+    memory_usage = torch.cuda.max_memory_allocated() / 1024 ** 2
+    torch.cuda.empty_cache()
+    torch.cuda.reset_peak_memory_stats()
 
-    seq_length = input_embd.size(0)
-    head_length = embedding_dim // num_heads
-
-    # We work on per-head sequences (no batching for now)
-    # Rearrange data as [num_heads, seq_length, head_length]
-    q2 = q1.view(seq_length, num_heads, head_length).transpose(0, 1)
-    k2 = k1.view(seq_length, num_heads, head_length).transpose(0, 1)
-    v2 = v1.view(seq_length, num_heads, head_length).transpose(0, 1)
-
-    # q * k_transposed / sqrt(dk)
-    dk = head_length
-    qk = q2.matmul(k2.transpose(1, 2)) / math.sqrt(dk)
-
-    # out = softmax(qk) * v (no out projection for now)
-    mh_attn = nn.functional.softmax(qk, dim=-1)
-    mh_attn_out = mh_attn.matmul(v2)
-
-    # Rearrange data back as [seq_length, embedding_dim]
-    mh_attn_out = mh_attn_out.transpose(0, 1).reshape(seq_length, embedding_dim)
-    return mh_attn_out
+    print(f'{title:16} END   Peak CUDA Mem: {memory_usage:.2f} MB')
+    print(f'{title:16}       Elapsed {(end - start) * 1000:.2f}ms')
+    return hidden
 
 
-# Does mean and std normalization, then applies learnable weight and bias
-norm_layer1 = nn.LayerNorm(embedding_dim)  # learnable
-norm_layer2 = nn.LayerNorm(embedding_dim)  # learnable
+def main():
+    #model_name = 'facebook/opt-350m'
+    model_name = 'mistralai/Mistral-7B-v0.1'
+    tokenizer = utils.get_tokenizer(model_name)
+    vocab_embedding = utils.get_vocab_embed(model_name)
+    prompts = utils.get_prompts()[:1]
 
-def multi_head_attention_add_and_norm_naive(input_embd: torch.Tensor):
-    return norm_layer1(input_embd)
+    tokens = tokenizer(prompts, padding=True, return_tensors='pt')
+    hidden_states = vocab_embedding(tokens.input_ids)
 
-def feed_forward_add_norm_naive(input_embd: torch.Tensor):
-    return norm_layer2(input_embd)
+    out1 = test_attn(hidden_states, mha_naive, 32, 100, 'NAIVE')
+    out2 = test_attn(hidden_states, mha_xformers, 32, 100, 'XFORMERS')
+    out3 = test_attn(hidden_states, mha_sdpa, 32, 100, 'SDPA')
+
+    mse1 = torch.mean((out1 - out2) ** 2)
+    mse2 = torch.mean((out1 - out3) ** 2)
+    mse3 = torch.mean((out2 - out3) ** 2)
+    print(f'\n{mse1}, {mse2}, {mse3}')
 
 
-ffn_linear1 = nn.Linear(embedding_dim, 4 * embedding_dim)
-ffn_linear2 = nn.Linear(4 * embedding_dim, embedding_dim)
-ffn_act = nn.ReLU()
-ffn_drop = nn.Dropout(0.1)  # discard random 10% to avoid overfit
-
-def feed_forward_naive(input_embd: torch.Tensor):
-    hidden_states = ffn_linear1(input_embd)
-    hidden_states = ffn_drop(ffn_act(hidden_states))
-    hidden_states = ffn_linear2(hidden_states)
-    return hidden_states
+main()
