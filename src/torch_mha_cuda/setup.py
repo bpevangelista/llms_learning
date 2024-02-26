aec5b8b86829f545af9e6a03de28da9aacda5e71
Bruno Evangelista <bpevangelista@gmail.com>
2024-02-25 21:24:43 -0800

    Start mha cuda

---
+++ src/torch_mha_cuda/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from torch.utils import cpp_extension
+
+ext_modules = [
+    cpp_extension.CUDAExtension(
+        name='mha',
+        sources=[
+            'cstr/mha.cpp',
+            'cstr/mha_kernel.cu',
+        ])
+]
+setup(
+    name='mha',
+    version='0.0.1',
+    description='',
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': cpp_extension.BuildExtension},
+)
