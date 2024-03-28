eb42609902fbe76d23a0a2bebaba6fe09d0f8a0b
Bruno Evangelista <bpevangelista@gmail.com>
2024-03-28 10:03:47 -0700

    Improved s2 to use generic MxK KxN matrix size

--- src/cuda_samples/gemm/s2_gemm_naive_templated.cu
+++ src/cuda_samples/gemm/s2_gemm_naive_templated.cu
@@ -1,23 +1,26 @@
+#include <assert.h>
 #include <cuda_fp16.h>
 #include <stdint.h>
 #include <stdio.h>
 #include "helpers.cu"
 
 
-// Parallel on output index [Rows, Rows] = [Rows, Cols] * [Cols, Rows]
-// matA is row-major and matB col-major for continuous memory access
+// One thread per output element [matSizeM, matSizeN] = [matSizeM, matSizeK] * [matSizeK, matSizeN]
+// matA (row-major) and matB (col-major) allows continuous memory access
 template <typename T, typename Acc>
-__global__ void gemm_kernel(const T* __restrict__ matA, const T* __restrict__ matB, T* __restrict__ matOut, const int32_t matRows, const int32_t matCols) {
+__global__ void gemm_kernel1x1(const T* __restrict__ matA, const T* __restrict__ matB, T* __restrict__ matOut,
+    const int32_t matSizeM, const int32_t matSizeN, const int32_t matSizeK) {
+
     // Using 1D block
     int32_t index = blockIdx.x * blockDim.x + threadIdx.x;
-    // Out is Rows x Rows
-    int32_t row = index / matRows;
-    int32_t col = index % matRows;
+    // Out is matSizeM x matSizeN
+    int32_t row = index / matSizeN;
+    int32_t col = index % matSizeN;
 
     Acc acc = static_cast<Acc>(0.0f);
-    for (int32_t k=0; k < matCols; k++) {
+    for (int32_t k=0; k < matSizeK; k++) {
         // Row-major * Column-major
-        acc = static_cast<Acc>(matA[row * matCols + k] * matB[col * matCols + k]) + acc;
+        acc = static_cast<Acc>(matA[row * matSizeK + k] * matB[col * matSizeK + k]) + acc;
     }
 
     matOut[index] = static_cast<T>(acc);
@@ -26,24 +29,28 @@ __global__ void gemm_kernel(const T* __restrict__ matA, const T* __restrict__ ma
 
 template <typename T, typename Acc>
 int gemm_main(float EPSILON = 0.001f) {
-     constexpr int32_t matRows = 2048; // 2k context
-     constexpr int32_t matCols = 4096; // 4k token dimension
+    // MxK and NxK matrices
+    constexpr int32_t matSizeM = 2048;      // 2k context
+    constexpr int32_t matSizeK = 4096;      // 4k token dimension
+    constexpr int32_t matSizeN = 2048;      // 2k context
 
     // Keep CPU copy of data for validation later
     T *cpuMatA, *cpuMatB, *cpuMatOut;
 
     // Alloc CUDA device memory with random data for matA, matB and zeros for matOut
-    T* matA = deviceTensorRand<T>(1, matRows, matCols, 2.0f, &cpuMatA);     // [-2, 2] rand values
-    T* matB = deviceTensorRand<T>(1, matCols, matRows, 2.0f, &cpuMatB);     // [-2, 2] rand values
+    T* matA = deviceTensorRand<T>(1, matSizeM, matSizeK, 2.0f, &cpuMatA);     // [-2, 2] rand values
+    T* matB = deviceTensorRand<T>(1, matSizeK, matSizeN, 2.0f, &cpuMatB);     // [-2, 2] rand values
     // Output is matRows x matRows
-    T* matOut = deviceTensorRand<T>(1, matRows, matRows, 0.0f, &cpuMatOut); // [ 0, 0] rand values
+    T* matOut = deviceTensorRand<T>(1, matSizeM, matSizeN, 0.0f, &cpuMatOut); // Zeroed
     if (matA == nullptr || matB == nullptr || matOut == nullptr) {
         return -1; // error
     }
 
     // Empiric block size of 128 threads (rational, SM can dispatch 4xWarps of 32 threads)
     dim3 blockSize = dim3(128, 1, 1);
-    dim3 blocksCount = dim3(ceil(matRows * matRows / float(blockSize.x)));
+    dim3 blocksCount = dim3((matSizeM * matSizeN) / blockSize.x);
+    // For simplicity, matrix size must be multiple of block size
+    assert((matSizeM * matSizeN) % blockSize.x == 0);
     int32_t sharedMemorySize = 0;
 
     // Calculate on GPU
@@ -54,22 +61,22 @@ int gemm_main(float EPSILON = 0.001f) {
     cudaEventCreate(&kernelStop);
 
     cudaEventRecord(kernelStart, 0);
-    gemm_kernel<T, Acc><<<blocksCount, blockSize, sharedMemorySize, stream>>>(matA, matB, matOut, matRows, matCols);
+    gemm_kernel1x1<T, Acc><<<blocksCount, blockSize, sharedMemorySize, stream>>>(matA, matB, matOut, matSizeM, matSizeN, matSizeK);
     cudaEventRecord(kernelStop, 0);
 
     // Calculate on CPU
-    for (int i=0; i<matRows; ++i) {
-        for (int j=0; j<matRows; ++j) {
+    for (int i=0; i<matSizeM; ++i) {
+        for (int j=0; j<matSizeN; ++j) {
             Acc acc = 0.0f;
-            for (int k=0; k<matCols; ++k) {
-                float temp = static_cast<float>(cpuMatA[i * matCols + k]) * static_cast<float>(cpuMatB[j * matCols + k]);
+            for (int k=0; k<matSizeK; ++k) {
+                float temp = static_cast<float>(cpuMatA[i * matSizeK + k]) * static_cast<float>(cpuMatB[j * matSizeK + k]);
                 if (sizeof(Acc) == 2) { // half
                     acc = __float2half_rn(static_cast<float>(acc) + temp);
                 } else { // float
                     acc += temp;
                 }
             }
-            cpuMatOut[i * matRows + j] = acc;
+            cpuMatOut[i * matSizeN + j] = acc;
         }
     }
 
@@ -85,8 +92,8 @@ int gemm_main(float EPSILON = 0.001f) {
     printf("Kernel runtime: %.2fms\n", kernelMs);
 
     // Validate CPU vs GPU computation
-    debugCompare<T>(cpuMatOut, matOut, nullptr, matRows * matRows, EPSILON);
-    printf("Deltas: %d %.3f\n", deltas, deltas/(float)(matSizeM*matSizeN));
+    auto [diffs, mse] = debugCompare<T>(cpuMatOut, matOut, nullptr, matSizeM * matSizeN, EPSILON);
+    printf("Epsilon-diffs: count %d, perc %.3f. MSE %.4f\n", diffs, diffs/(float)(matSizeM * matSizeN), mse);
 
     SAFE_FREE(cpuMatA);
     SAFE_FREE(cpuMatB);
@@ -98,7 +105,7 @@ int gemm_main(float EPSILON = 0.001f) {
 }
 
 int main() {
-    //return gemm_main<float, float>(0.001);    // 950ms on 3060TI for 2k-4k-gemm
-    return gemm_main<half, float>(0.25f);       // 300ms on 3060TI for 2k-4k-gemm
+    //return gemm_main<float, float>(0.001f);    // 950ms on 3060TI for 2k-4k-gemm
+    return gemm_main<half, float>(0.1f);       // 300ms on 3060TI for 2k-4k-gemm
     //return gemm_main<half, half>(0.75f);      // 360ms on 3060TI for 2k-4k-gemm
 }
