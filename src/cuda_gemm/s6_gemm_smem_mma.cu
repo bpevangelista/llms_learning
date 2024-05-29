d78519122352fda753b685783e4a6416cc78abde
Bruno Evangelista <bpevangelista@gmail.com>
2024-05-29 15:42:09 -0700

    slow but working cp.async smem

--- src/cuda_gemm/s6_gemm_smem_mma.cu
+++ src/cuda_gemm/s6_gemm_smem_mma.cu
@@ -7,190 +7,208 @@ __device__ __forceinline__ uint32_t lane_id() { uint32_t res; asm("mov.u32 %0, %
 __device__ float f1(uint32_t val) { return __half2float( ((half*)&val)[0] ); }
 __device__ float f2(uint32_t val) { return __half2float( ((half*)&val)[1] ); }
 
-__device__ float cp_async16B(void* smem, void *gmem) {
-    asm("cp.async.ca.shared.global")
+__device__ void cp_async16B_cache_all(void *smem_ptr, const void *gmem_ptr, bool predicate=true) {
+    constexpr int32_t kCopySize = 16;
+    int32_t src_size = predicate? kCopySize : 0;
+    uint32_t smem_ptr_u32 = static_cast<uint32_t>(__cvta_generic_to_shared(smem_ptr)); // Cast smem_ptr to u32
+
+    asm volatile("cp.async.ca.shared.global [%0], [%1], %2, %3;"
+        :: "r"(smem_ptr_u32), "l"(gmem_ptr), "n"(kCopySize), "r"(src_size));
 }
 
-// One thread per output element [matSizeM, matSizeN] = [matSizeM, matSizeK] * [matSizeK, matSizeN]
-// matA (row-major) and matB (col-major) allows continuous memory access
-template <
-    int32_t matSizeM, int32_t matSizeN, int32_t matSizeK                // Matrix A & B sizes
->
-__global__ void gemm_kernel_m16n8k16(const half* __restrict__ matA, const half* __restrict__ matB, half* __restrict__ matOut) {
+__device__ void cp_async16B_cache_global(void *smem_ptr, const void *gmem_ptr, bool predicate=true) {
+    constexpr int32_t kCopySize = 16;
+    int32_t src_size = predicate? kCopySize : 0;
+    uint32_t smem_ptr_u32 = static_cast<uint32_t>(__cvta_generic_to_shared(smem_ptr)); // Cast smem_ptr to u32
 
-    constexpr int32_t kernelSizeMK = 16;
-    constexpr int32_t kernelSizeN = 8;
+    asm volatile("cp.async.cg.shared.global [%0], [%1], %2, %3;"
+        :: "r"(smem_ptr_u32), "l"(gmem_ptr), "n"(kCopySize), "r"(src_size));
+}
+
+__device__ void cp_async_wait_all() {
+    asm volatile("cp.async.wait_all;");
+}
+
+
+// One thread per output element [kMatSizeM, kMatSizeN] = [kMatSizeM, kMatSizeK] * [kMatSizeK, kMatSizeN]
+// mat_a (row-major) and mat_b (col-major) allows continuous memory access
+template <
+    int32_t kMatSizeM, int32_t kMatSizeN, int32_t kMatSizeK                // Matrix A & B sizes
+>
+__global__ void gemm_kernel_m16n8k16(const half* __restrict__ mat_a, const half* __restrict__ mat_b, half* __restrict__ mat_out) {
+
+    constexpr int32_t kKernelSizeMK = 16;
+    constexpr int32_t kKernelSizeN = 8;
 
     // 2D thread group block in 2D dispatch grid
-    int32_t row = blockIdx.x * kernelSizeMK;
-    int32_t col = blockIdx.y * kernelSizeN;
+    int32_t row = blockIdx.x * kKernelSizeMK;
+    int32_t col = blockIdx.y * kKernelSizeN;
 
-    // Handle output matrix (matSizeM x matSizeN) not evenly divisible by kernel size
-    if (row >= matSizeM || col >= matSizeN) {
+    // Handle output matrix (kMatSizeM x kMatSizeN) not evenly divisible by kernel size
+    if (row >= kMatSizeM || col >= kMatSizeN) {
         return; // Early exit
     }
 
+    uint32_t laneid = lane_id();
+    uint32_t shift_k = laneid >> 2;
+    uint32_t shift_elem = laneid % 4;
+
+    constexpr int32_t kBufferCount = 2; // Triple buffering
+    __shared__ uint32_t smem_mat_a[kKernelSizeMK*kKernelSizeMK*kBufferCount/2];
+    __shared__ uint32_t smem_mat_b[kKernelSizeMK*kKernelSizeN*kBufferCount/2];
+
     // MatOut 16x8 tile
     // Calculate 4xf32 p/thread
-    float acc0 = 0.0f;
-    float acc1 = 0.0f;
-    float acc2 = 0.0f;
-    float acc3 = 0.0f;
+    float acc[4] = {};
 
-    __shared__ T sharedMatA[kernelSizeMK*kernelSizeMK];
-    __shared__ T sharedMatB[kernelSizeMK*kernelSizeN];
-
-    uint32_t laneid = lane_id();
-    uint32_t k_shift = laneid >> 2;
-    uint32_t lane_shift = laneid % 4;
-
-    constexpr int32_t kernelSizeK = 16;
-    for (int32_t k=0; k < matSizeK; k += kernelSizeK) {
+    for (int32_t k=0; k < kMatSizeK; k += kKernelSizeMK) {
         // MatA 16x16 tile
         // 8xf16 across 4 32b register (2xf16 p/register)
-        uint32_t* matA_top_u32 = (uint32_t*)&matA[(row + k_shift + 0) * matSizeK + k];
-        uint32_t* matA_bot_u32 = (uint32_t*)&matA[(row + k_shift + 8) * matSizeK + k];
-        uint32_t a0 = matA_top_u32[lane_shift];
-        uint32_t a1 = matA_bot_u32[lane_shift];
-        uint32_t a2 = matA_top_u32[lane_shift + 4]; // step 8 - 4x(2xf16)
-        uint32_t a3 = matA_bot_u32[lane_shift + 4]; // step 8 - 4x(2xf16)
+        #pragma unroll
+        for (int cp = 0; cp < 16; cp++) {
+            uint32_t* mat_a_u32 = (uint32_t*)&mat_a[(row + cp) * kMatSizeK + k];
+            cp_async16B_cache_global(&smem_mat_a[cp * 8 + 0], &mat_a_u32[0]); // copy 8x
+            cp_async16B_cache_global(&smem_mat_a[cp * 8 + 4], &mat_a_u32[4]); // copy 8x
+        }
 
         // MatB 16x8 tile
         // 4xf16 across 2 32b register (2xf16 p/register)
-        uint32_t* matB_u32 = (uint32_t*)&matB[(col + k_shift + 0) * matSizeK + k];
-        uint32_t b0 = matB_u32[lane_shift];
-        uint32_t b1 = matB_u32[lane_shift + 4]; // step 8 - 4x(2xf16)
+        #pragma unroll
+        for (int cp = 0; cp < 8; cp++) {
+            uint32_t* mat_b_u32 = (uint32_t*)&mat_b[(col + cp) * kMatSizeK + k];
+            cp_async16B_cache_global(&smem_mat_b[cp * 8 + 0], &mat_b_u32[0]);
+            cp_async16B_cache_global(&smem_mat_b[cp * 8 + 4], &mat_b_u32[4]);
+        }
+
+        cp_async_wait_all();
+
+        uint32_t a0 = smem_mat_a[shift_k * 8 + shift_elem];
+        uint32_t a1 = smem_mat_a[shift_k * 8 + shift_elem + 64];
+        uint32_t a2 = smem_mat_a[shift_k * 8 + shift_elem + 4];
+        uint32_t a3 = smem_mat_a[shift_k * 8 + shift_elem + 64 + 4];
+        uint32_t b0 = smem_mat_b[shift_k * 8 + shift_elem];
+        uint32_t b1 = smem_mat_b[shift_k * 8 + shift_elem + 4];
 
         asm volatile(
           "mma.sync.aligned.m16n8k16.row.col.f32.f16.f16.f32 "
           "{%0,  %1,  %2,  %3},"
-          "{%4,  %5,  %6,  %7},"
-          "{%8,  %9},"
+          "{%4,  %5,  %6,  %7}, {%8,  %9},"
           "{%10, %11, %12, %13};\n"
-          : "=f"(acc0), "=f"(acc1), "=f"(acc2), "=f"(acc3)
-          :  "r"(a0),  "r"(a1),  "r"(a2),  "r"(a3),
-             "r"(b0),  "r"(b1),
-             "f"(acc0),  "f"(acc1),  "f"(acc2),  "f"(acc3));
+          : "=f"(acc[0]), "=f"(acc[1]), "=f"(acc[2]), "=f"(acc[3])
+          :  "r"(a0),  "r"(a1),  "r"(a2),  "r"(a3), "r"(b0),  "r"(b1),
+             "f"(acc[0]),  "f"(acc[1]),  "f"(acc[2]),  "f"(acc[3]));
 
-        // Debug
-        //if (laneid == 0) {
-        //    printf("\na %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f", f1(a0), f2(a0), f1(a1), f2(a1), f1(a2), f2(a2), f1(a3), f2(a3));
-        //    printf("\nb %.3f %.3f %.3f %.3f", f1(b0), f2(b0), f1(b1), f2(b1));
-        //    printf("\nacc %.3f %.3f %.3f %.3f\n\n", acc0, acc1, acc2, acc3);
-        //}
+#if 0 // Debug
+        if (laneid == 0) {
+            printf("\na %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f", f1(a0), f2(a0), f1(a1), f2(a1), f1(a2), f2(a2), f1(a3), f2(a3));
+            printf("\nb %.3f %.3f %.3f %.3f", f1(b0), f2(b0), f1(b1), f2(b1));
+            printf("\nacc %.3f %.3f %.3f %.3f\n\n", acc[0], acc[1], acc[2], acc[3]);
+        }
+#endif
     }
 
-    half* matOut_top = &matOut[(row + k_shift + 0) * matSizeN + col];
-    half* matOut_bot = &matOut[(row + k_shift + 8) * matSizeN + col];
-    matOut_top[lane_shift * 2 + 0] = __float2half_rn(acc0);
-    matOut_top[lane_shift * 2 + 1] = __float2half_rn(acc1);
-    matOut_bot[lane_shift * 2 + 0] = __float2half_rn(acc2);
-    matOut_bot[lane_shift * 2 + 1] = __float2half_rn(acc3);
+    half* mat_out_top = &mat_out[(row + shift_k + 0) * kMatSizeN + col];
+    half* mat_out_bot = &mat_out[(row + shift_k + 8) * kMatSizeN + col];
+    mat_out_top[shift_elem * 2 + 0] = __float2half_rn(acc[0]);
+    mat_out_top[shift_elem * 2 + 1] = __float2half_rn(acc[1]);
+    mat_out_bot[shift_elem * 2 + 0] = __float2half_rn(acc[2]);
+    mat_out_bot[shift_elem * 2 + 1] = __float2half_rn(acc[3]);
 }
 
 
 template <typename T, typename Acc>
 int gemm_main(float EPSILON = 0.001f) {
     // MxK and NxK matrices
-//#define USE_SMALL_MAT_FOR_DEBUG
-#ifdef USE_SMALL_MAT_FOR_DEBUG
-    constexpr int32_t matSizeM = 16;
-    constexpr int32_t matSizeK = 16;
-    constexpr int32_t matSizeN = 8;
-#else
-    constexpr int32_t matSizeM = 2048; // 2k context
-    constexpr int32_t matSizeK = 4096; // 4k token dimension
-    constexpr int32_t matSizeN = 2048; // 2k context
-#endif
+    constexpr int32_t kMatSizeM = 2048; // 2k context
+    constexpr int32_t kMatSizeK = 4096; // 4k token dimension
+    constexpr int32_t kMatSizeN = 2048; // 2k context
+
     // Per-Kernel Computation Size
-    constexpr int32_t kernelSizeM = 16;
-    constexpr int32_t kernelSizeN = 8;
+    constexpr int32_t kKernelSizeMK = 16;
+    constexpr int32_t kKernelSizeN = 8;
 
     // Keep CPU copy of data for validation later
-    T *cpuMatA, *cpuMatB, *cpuMatOut;
+    T *cpu_mat_a, *cpu_mat_b, *cpu_mat_out;
 
-    // Alloc CUDA device memory with random data for matA, matB and zeros for matOut
-    T* matA = deviceTensorRand<T>(1, matSizeM, matSizeK, 2.0f, &cpuMatA);     // [-2, 2] rand values
-    T* matB = deviceTensorRand<T>(1, matSizeK, matSizeN, 2.0f, &cpuMatB);     // [-2, 2] rand values
+    // Alloc CUDA device memory with random data for mat_a, mat_b and zeros for mat_out
+    T* mat_a = deviceTensorRand<T>(1, kMatSizeM, kMatSizeK, 2.0f, &cpu_mat_a);     // [-2, 2] rand values
+    T* mat_b = deviceTensorRand<T>(1, kMatSizeK, kMatSizeN, 2.0f, &cpu_mat_b);     // [-2, 2] rand values
     // Output is matRows x matRows
-    T* matOut = deviceTensorRand<T>(1, matSizeM, matSizeN, 0.0f, &cpuMatOut); // [0, 0] Zero it
-    if (matA == nullptr || matB == nullptr || matOut == nullptr) {
+    T* mat_out = deviceTensorRand<T>(1, kMatSizeM, kMatSizeN, 0.0f, &cpu_mat_out); // [0, 0] Zero it
+    if (mat_a == nullptr || mat_b == nullptr || mat_out == nullptr) {
         return -1; // error
     }
 
     // Must be warp-size (32) multiple due to mma instruction
-    dim3 threadGroupSize = dim3(32, 1, 1);
-    dim3 threadGroupsCount = dim3(
-        CEIL_DIV(matSizeM, kernelSizeM),
-        CEIL_DIV(matSizeN, kernelSizeN)
-        );
-    int32_t dynamicSharedMemSize = 0; // Still not using it
+    dim3 thread_group_size = dim3(32, 1, 1);
+    dim3 thread_groups = dim3(
+        CEIL_DIV(kMatSizeM, kKernelSizeMK),
+        CEIL_DIV(kMatSizeN, kKernelSizeN));
+    int32_t dynamic_smem_size = 0;
 
     // Calculate on GPU
     cudaStream_t stream;
-    cudaEvent_t kernelStart, kernelStop;
+    cudaEvent_t kernel_start, kernel_stop;
     cudaStreamCreate(&stream);
-    cudaEventCreate(&kernelStart);
-    cudaEventCreate(&kernelStop);
+    cudaEventCreate(&kernel_start);
+    cudaEventCreate(&kernel_stop);
 
-    cudaEventRecord(kernelStart, 0);
+    cudaEventRecord(kernel_start, stream);
     gemm_kernel_m16n8k16
-        <matSizeM, matSizeN, matSizeK>
-        <<<threadGroupsCount, threadGroupSize, dynamicSharedMemSize, stream>>>
-        (matA, matB, matOut);
-    cudaEventRecord(kernelStop, 0);
+        <kMatSizeM, kMatSizeN, kMatSizeK>
+        <<<thread_groups, thread_group_size, dynamic_smem_size, stream>>>
+        (mat_a, mat_b, mat_out);
+    cudaEventRecord(kernel_stop, stream);
 
+    // Wait for GPU (just for correctness as CPU is much slower)
+    cudaError_t status = cudaStreamSynchronize(stream);
+    cudaStreamDestroy(stream);
+
+    // Calculate runtime (ideally avg over many runs)
+    float kernel_elapsed_ms = 0.0f;
+    cudaEventElapsedTime(&kernel_elapsed_ms, kernel_start, kernel_stop);
+    cudaEventDestroy(kernel_start);
+    cudaEventDestroy(kernel_stop);
+    printf("Kernel runtime: %.2fms\n", kernel_elapsed_ms);
+
+#ifdef CPU_MATH_VALIDATION_ENABLED
     // Calculate on CPU
-    for (int i=0; i<matSizeM; ++i) {
-        for (int j=0; j<matSizeN; ++j) {
+    for (int i=0; i < kMatSizeM; ++i) {
+        for (int j=0; j < kMatSizeN; ++j) {
             Acc acc = 0.0f;
-            for (int k=0; k<matSizeK; ++k) {
-                float temp = static_cast<float>(cpuMatA[i * matSizeK + k]) * static_cast<float>(cpuMatB[j * matSizeK + k]);
+            for (int k=0; k < kMatSizeK; ++k) {
+                float temp = static_cast<float>(cpu_mat_a[i * kMatSizeK + k]) * static_cast<float>(cpu_mat_b[j * kMatSizeK + k]);
                 if (sizeof(Acc) == 2) { // half
                     acc = __float2half_rn(static_cast<float>(acc) + temp);
                 } else { // float
                     acc += temp;
                 }
             }
-            cpuMatOut[i * matSizeN + j] = acc;
+            cpu_mat_out[i * kMatSizeN + j] = acc;
         }
     }
 
-    // Wait for GPU (just for correctness as CPU is much slower)
-    cudaError_t cudaStatus = cudaStreamSynchronize(stream);
-    cudaStreamDestroy(stream);
-
-    // Calculate runtime (ideally avg over many runs)
-    float kernelMs = 0.0f;
-    cudaEventElapsedTime(&kernelMs, kernelStart, kernelStop);
-    cudaEventDestroy(kernelStart);
-    cudaEventDestroy(kernelStop);
-    printf("Kernel runtime: %.2fms\n", kernelMs);
-
     // Validate CPU vs GPU computation
-    T* matOutCpuPtr;
-    auto [diffs, mse] = debugCompare(cpuMatOut, matOut, &matOutCpuPtr, matSizeM * matSizeN, EPSILON);
-    printf("Epsilon-diffs: count %d, perc %.3f, MSE %.4f\n", diffs, diffs/(float)(matSizeM * matSizeN), mse);
+    T* mat_out_cpu_copied;
+    auto [diffs, mse] = debugCompare(cpu_mat_out, mat_out, &mat_out_cpu_copied, kMatSizeM * kMatSizeN, EPSILON);
+    printf("Epsilon-diffs: count %d, perc %.3f, MSE %.4f\n", diffs, diffs/(float)(kMatSizeM * kMatSizeN), mse);
 
     // Debug small matrices
-    if (matSizeM <= 32 && matSizeN <= 32) {
-        printf("cpuMatA\n");
-        printTensor(cpuMatA, matSizeM, matSizeK);
-        printf("cpuMatB\n");
-        printTensor(cpuMatB, matSizeN, matSizeK);
-        printf("cpuMatOut\n");
-        printTensor(cpuMatOut, matSizeM, matSizeN);
-        printf("cudaMatOut\n");
-        printTensor(matOutCpuPtr, matSizeM, matSizeN);
+    if (kMatSizeM <= 32 && kMatSizeN <= 32) {
+        printTensor("cpu_mat_a\n", cpu_mat_a, kMatSizeM, kMatSizeK);
+        printTensor("cpu_mat_b\n", cpu_mat_b, kMatSizeN, kMatSizeK);
+        printTensor("cpu_mat_out\n", cpu_mat_out, kMatSizeM, kMatSizeN);
+        printTensor("cuda_mat_out\n", mat_out_cpu_copied, kMatSizeM, kMatSizeN);
     }
+    SAFE_FREE(mat_out_cpu_copied);
+#endif
 
-    SAFE_FREE(cpuMatA);
-    SAFE_FREE(cpuMatB);
-    SAFE_FREE(cpuMatOut);
-    SAFE_FREE(matOutCpuPtr);
-    SAFE_CUDA_FREE(matA);
-    SAFE_CUDA_FREE(matB);
-    SAFE_CUDA_FREE(matOut);
+    SAFE_FREE(cpu_mat_a);
+    SAFE_FREE(cpu_mat_b);
+    SAFE_FREE(cpu_mat_out);
+    SAFE_CUDA_FREE(mat_a);
+    SAFE_CUDA_FREE(mat_b);
+    SAFE_CUDA_FREE(mat_out);
     return 0;
 }
 
